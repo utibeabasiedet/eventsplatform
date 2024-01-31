3c174b43a921eb6fde9facaaa2ac7b8a6f77ff0f
utibe edet <utibeabasi.okon@myrecharge.ng>
2024-01-30 17:27:43 -0800

    second commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,23 @@
+// import CategoryFilter from '@/components/shared/CategoryFilter';
+import Collection from '@/components/shared/Collection'
+// import Search from '@/components/shared/Search';
 import { Button } from '@/components/ui/button'
+ import { getAllEvents } from '@/lib/actions/event.actions';
+import { SearchParamProps } from '@/types';
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || '';
+  const category = (searchParams?.category as string) || '';
+
+  const events = await getAllEvents({
+    query: searchText,
+    category,
+    page,
+    limit: 6
+  })
   return (
     <>
     <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
@@ -26,6 +41,7 @@ export default function Home() {
           />
         </div>
       </section> 
+
       <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
         <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
 
@@ -34,7 +50,7 @@ export default function Home() {
           <CategoryFilter /> */}
         </div>
 
-        {/* <Collection 
+        <Collection 
           data={events?.data}
           emptyTitle="No Events Found"
           emptyStateSubtext="Come back later"
@@ -42,7 +58,7 @@ export default function Home() {
           limit={6}
           page={page}
           totalPages={events?.totalPages}
-        /> */}
+        />
       </section>
     </>
     
